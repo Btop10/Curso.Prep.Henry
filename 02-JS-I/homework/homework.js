410babe991a6892b516d809d826e13cb48aafbd1
Btop10 <btoperalta@gmail.com>
2022-08-02 20:15:43 -0500

    ultimo commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,7 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  devolver cadena ;
+  devolver;
 }
 
 function suma(x, y) {
@@ -191,27 +191,27 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  volver  'Hola'  +  nombre  +  '!' ;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  volver  alto  *  ancho ;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  lado de  retorno *  4
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  retorno  ( base  *  altura ) / 2
 }
 
 
@@ -219,7 +219,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  vuelta  euro  *  1,2
 }
 
 
@@ -229,9 +229,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if ( letra . longitud  >  1 ) {
+    devolver  "Dato incorrecto"
+  }
+  if ( letra  ===  "a"  ||  letra  ===  "e"  ||  letra  ===  "i"  ||  letra  ===  "o"  ||  letra  ===  "u" ) {
+    volver  "Es vocal"
+  }
+  devolver  "Dato incorrecto"
 }
-
 
 
 // No modificar nada debajo de esta línea
